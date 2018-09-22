88b0becdb1f5dd3e021aa41b8fa01e5926c4cd72
yuukiishikawa <yuuki.ishikawa@mercury.ne.jp>
2018-09-22 17:48:14 +0100

    QR code things

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -6,14 +6,52 @@ import QrReader from 'react-qr-reader'
 import ReactDOM from 'react-dom';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      delay: 300,
+      result: 'No result',
+    }
+    this.handleScan = this.handleScan.bind(this)
+  }
+  handleScan(data){
+    if(data){
+      this.setState({
+        result: data,
+      })
+    }
+  }
+  handleError(err){
+    console.error(err)
+  }
   render() {
-      return (<QRCode
+      return (
+      <div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to EHandShake</h1>
+        </header>
+        <p className="App-intro">
+          EHandShake
+        </p>
+        <p>
+        <QRCode
               bgColor="#FFFFFF"
               fgColor="#000000"
               level="Q"
               style={{ width: 256 }}
               value="some text -eadewadawdwaedaedewa"
-          />);
+          />
+          <QrReader
+            delay={this.state.delay}
+            onError={this.handleError}
+            onScan={this.handleScan}
+            style={{ width: '50%' }}
+            />
+          <p>{this.state.result}</p>
+        </p>
+      </div>
+      );
   }
 }
 
